b583fd89d4f380596775fcdc9c071aea9dbb3025
NawidT <nawidtahmid@gmail.com>
2025-03-01 23:19:08 -0500

    combined agent + playwright

--- agent.py
+++ agent.py
@@ -4,10 +4,11 @@ from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, BaseMessage
 from langchain_openai import ChatOpenAI
 import os
-from playwright.sync_api import Page
+from playwright.async_api import Page
+from typing_extensions import TypedDict
+import asyncio
 
 class RT_State(TypedDict):
-    page : object
     selected_element : str
     context_html : str
     context_img : str
@@ -15,16 +16,18 @@ class RT_State(TypedDict):
 
 class RT_Graph(StateGraph):
     def __init__(self):
-        super().__init__()
+        super().__init__(RT_State)
         # add the nodes
         self.add_node("set_selected_element", self.set_selected_element)
         self.add_node("set_context_html", self.set_context_html)
         self.add_node("set_context_img", self.set_context_img)
         self.add_node("decide_if_selected_is_message_box", self.decide_if_selected_is_message_box)
+        self.add_node("wait", self.handle_wait)
         # add the chat
         self.chat = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
         self.chat = self.chat.bind_tools([self.set_context_html, self.set_context_img])
         # add the edges
+        self.add_edge(START, "set_selected_element")
         self.add_edge("set_selected_element", "decide_if_selected_is_message_box")
         self.add_edge("set_context_html", "decide_if_selected_is_message_box")
         self.add_edge("set_context_img", "decide_if_selected_is_message_box")
@@ -32,40 +35,45 @@ class RT_Graph(StateGraph):
             "decide_if_selected_is_message_box",
             self.cond_edge_is_messbox,
         )
-        # compile the graph
-        self.compile()
+        self.add_edge("wait", "set_selected_element")
+        # additional attributes
+        self.page = None
+
 
     def set_page(self, page : Page):
         self.page = page
 
-    def set_selected_element(self, state : RT_State):
+    async def set_selected_element(self, state : RT_State):
         """ Sets the tag name of the element currently selected by the mouse om the selected_element field of the state """
-        print("set selected element")
-        if isinstance(state['page'], Page):
-            hover_element = state['page'].locator(':hover')
-            selected_elem = hover_element.evaluate('e => e.tagName')
+        if self.page and isinstance(self.page, Page):
+            selected_elem = await self.page.evaluate("document.activeElement?.tagName?.toLowerCase() || ''")
             state['selected_element'] = selected_elem
+        print("page: ", self.page)
+        print("set selected element: ", selected_elem)
         return state
 
     @tool
-    def set_context_html(self, state : RT_State):
+    async def set_context_html(self, state : RT_State):
         """ Sets the inner html of the element currently selected by the mouse on the context_html field of the state """
         print("get context html")
-        if  isinstance(state['page'], Page):
-            context_html = state['page'].evaluate('document.querySelector(":hover").innerHTML')
+        if self.page and isinstance(self.page, Page):
+            context_html = await self.page.evaluate("document.activeElement?.outerHTML || ''    ")
             state['context_html'] = context_html
-            state['messages'].append(SystemMessage(content=f"Here is the inner html of the element: {context_html}"))
+            state['messages'].append(HumanMessage(content=f"Here is the inner html of the element: {context_html}"))
         return state
 
     @tool
     def set_context_img(self, state : RT_State):
         """ Sets the image of the element currently selected by the mouse on the context_img field of the state """
         print("get context img")
-        if isinstance(state['page'], Page):
-            context_img = state['page'].screenshot()
+        if self.page and isinstance(self.page, Page):
+            context_img = self.page.screenshot()
             # pass thru an LLM to describe the image
-            state['context_img'] = context_img
-            state['messages'].append(SystemMessage(content=f"Here is the description of the image of the element: {context_img}"))
+            llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
+            img_desc = llm.invoke(context_img)
+            state['context_img'] = img_desc
+            print("img_desc: ", img_desc)
+            state['messages'].append(HumanMessage(content=f"Here is the description of the image of the element: {img_desc}"))
         return state
         
     def decide_if_selected_is_message_box(self, state : RT_State):
@@ -77,25 +85,38 @@ class RT_Graph(StateGraph):
                 Name of the selected element: {selected_element}
                     
                 You have the following tools at your disposal:
-                set_context_html - to find the html of the page if you don't have it yet
-                set_context_img - to find the screenshot of the page if you don't have it yet
+                set_context_html - to find the html of the page
+                set_context_img - to find the screenshot of the page
                     
-                If you think the selected element is a message box, return True. 
+                If you think the selected element is a text box, return True 
                 If you think you need to use a tool, return the name of the tool
-                Otherwise, return False.
+                Otherwise, return False
         """.format(selected_element=state['selected_element']))
         )
         state['messages'].append(
             HumanMessage(content="Is the selected element a message box?")
         )
         ans = self.chat.invoke(state['messages'])
+        print("ans: ", ans.content.strip())
         state['messages'].append(ans)
         return state
 
     def cond_edge_is_messbox(self, state : RT_State):
         if state['messages'][-1].tool_calls:
             print("tool call")
+            print(state['messages'][-1].tool_calls)
             return state['messages'][-1].tool_calls[0].name
+        elif state['messages'][-1].content.strip() == "True":
+            print("THIS IS A MESSAGE BOX")
+            return "wait"
         else:
             print("no tool call")
             return "wait"
+        
+    async def handle_wait(self, state : RT_State):
+        print("waiting")
+        # keep only the last 4 messages to get historical context
+        state['messages'] = state['messages'][-4:]
+        # print("Chat so far: ", [message.content + " -> " for message in state['messages']])
+        await asyncio.sleep(10)
+        return state
